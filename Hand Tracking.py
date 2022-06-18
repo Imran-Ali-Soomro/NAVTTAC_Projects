f04d97c195ca043a0b0f4a6fde7ee51d47a25264
Imran Ali <64371342+Imran-Ali-Soomro@users.noreply.github.com>
2022-06-18 15:13:52 +0500

    Add files via upload

---
+++ Hand Tracking.py
@@ -0,0 +1,38 @@
+import mediapipe as mp
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+cTime = 0
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+
+    if result.multi_hand_landmarks:
+        for handlms in result.multi_hand_landmarks:
+            for id, lm in enumerate(handlms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                cv2.circle(img, (cx, cy), 8, (255, 0, 0), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow('Frames', img)
+    cv2.waitKey(1)
+
